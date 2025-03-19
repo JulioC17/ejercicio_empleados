0e302035b48639ce648f98304a9880200fe33ffd
Julio <juliocsreyes94@gmail.com>
2025-03-19 18:08:42 +0100

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Empleado empleado1 = new Empleado("Juanito");
+        Operario empleado2 = new Operario("Alberto");
+        Directivo empleado3  = new Directivo("Julio");
+
+        System.out.println(empleado1);
+        System.out.println(empleado2);
+        System.out.println(empleado3);
     }
 }
